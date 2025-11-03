f29460b774747453cdbd218b01b9776b675da2b1
farzan ghalami <ghalamif@gmail.com>
2025-11-03 13:43:31 +0100

    Fix indentation in Printable interface documentation

--- src/adap-b02/common/Printable.ts
+++ src/adap-b02/common/Printable.ts
@@ -3,25 +3,25 @@ export const ESCAPE_CHARACTER = '\\';
 
 export interface Printable {
 
-   /**
+  /**
      * Returns a human-readable representation of the Name instance using user-set special characters
      * Special characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
      * The delimiter character must be a single character
      */
-   asString(delimiter?: string): string;
+  asString(delimiter?: string): string;
 
-   /** 
+  /** 
     * Returns a machine-readable representation of Name instance using default special characters
     * Machine-readable means that from a data string, a Name can be parsed back in
     * The special characters in the data string are the default characters
     * Different fields of the object are separated by the delimiter character
     */
-   asDataString(): string;
+  asDataString(): string;
 
-   /**
+  /**
     * Returns delimiter char; must be a single character
     */
-   getDelimiterCharacter(): string;
+  getDelimiterCharacter(): string;
 
 }
